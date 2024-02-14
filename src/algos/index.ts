642834d2ec5f910aa30784469e0aef7c08532836
Andromeda Yelton <andromeda.yelton@gmail.com>
2024-02-14 11:19:55 -0500

    Add skybrarians algo

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,14 +3,14 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as forScience from './for-science'
+import * as skybrarians from './skybrarians'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos = {
-  [forScience.shortname]: {
-    handler: <AlgoHandler>forScience.handler,
-    manager: forScience.manager,
+  [skybrarians.shortname]: {
+    handler: <AlgoHandler>skybrarians.handler,
+    manager: skybrarians.manager,
   },
 }
 
